02f719fd0da7d0bb867cba371f49ddeeb293e223
avila-ca <avilacalaf@gmail.com>
2023-11-01 18:37:27 +0100

    query 28 done

--- src/queries.ts
+++ src/queries.ts
@@ -1,3 +1,4 @@
+import { ReadStream } from "fs";
 import mongoose from "mongoose";
 
 export const queries = async (collectionName:string) =>{
@@ -16,8 +17,24 @@ export const queries = async (collectionName:string) =>{
         //const query11 = await Restaurant.find({cuisine:{$eq:"American"}, "grades.score":{$gt:70}, "address.coord": {$lt:-65.754168}})
         //const query12 = await Restaurant.find({cuisine:{$ne:"American"}, "grades.score":{$gt:70}, "address.coord": {$lt:-65.754168}})
         //const query13 = await Restaurant.find({cuisine:{$ne:"American"}, "grades.grade":{$eq:"A"}, borough:{$ne:"Brooklyn"}}).sort({cuisine:-1});
-        const query14 = await Restaurant.find({ name:/^Wil/i }, {restaurant_id:1, cuisine:1, name:1})
-        console.log(query14);
+        //const query14 = await Restaurant.find({ name:/^Wil/i }, {restaurant_id:1, cuisine:1, name:1})
+        //const query15 = await Restaurant.find({ name:/ces$/i }, {restaurant_id:1, cuisine:1, name:1})
+        //const query16 = await Restaurant.find({ name:/Reg/i }, {restaurant_id:1, cuisine:1, name:1})
+        //const query17 = await Restaurant.find({borough:"Bronx", cuisine:{$in:["American","Chinese"]}});
+        //const query18 = await Restaurant.find({ borough:{$in: ["Staten Island", "Queens", "Bronx", "Brooklyn"]} }, {restaurant_id:1, cuisine:1, name:1, borough:1})
+        //const query19 = await Restaurant.find({ borough:{$nin: ["Staten Island", "Queens", "Bronx", "Brooklyn"]} }, {restaurant_id:1, cuisine:1, name:1, borough:1})
+        //const query20 = await Restaurant.find({ "grades.score":{$lt: 10}}, {restaurant_id:1, cuisine:1, name:1, borough:1})
+        //const query21 = await Restaurant.find({ cuisine:"seafood", $nor: [{cuisine: "American"}, {cuisine:"Chinese"}, {name: /^Wil/i}]}, {restaurant_id:1, cuisine:1, name:1, borough:1})
+        //const query22 = await Restaurant.find({"grades": {$elemMatch:{"grade":"A", "score": 11, "date": "2014-08-11T00:00:00Z"}}}, {restaurant_id:1, grades:1, name:1, borough:1});
+        //const query23 = await Restaurant.find({"grades": {$elemMatch:{"grade":"A", "score": 9, "date": "2014-08-11T00:00:00Z"}}}, {restaurant_id:1, grades:1, name:1, borough:1});
+        //const query24 = await Restaurant.find({"address.coord.1":{$gt:42, $lt:52}},{restaurant_id:1, name:1, "address.street":1, "address.coord.0":1, "address.coord.1":1});
+        //const query25 = await Restaurant.find({}, {name:1, _id:0}).sort({"name":1});
+        //const query26 = await Restaurant.find({}, {name:1, _id:0}).sort({"name":-1});
+        //const query27 = await Restaurant.find({name:1, _id:0}).sort({cuisine:-1, borough: 1});
+        //const query28 = await Restaurant.find({"address.street":{$esists:true}});
+        const query29 = await Restaurant.find({},{})
+
+        console.log(query29);
     }
     catch(err){
         console.log("Error query consulting: ", err);
